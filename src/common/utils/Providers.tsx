c871ae10073ca237d9006093b7a141c9f2a01dea
dangtinh <caodangtinh04022003@gmail.com>
2024-02-03 15:58:46 +0700

    feat/PJ-02 : register UI

--- src/common/utils/Providers.tsx
+++ src/common/utils/Providers.tsx
@@ -1,18 +1,20 @@
 "use client";
-
 import React from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+import StoreProvider from "../lib/StoreProvider";
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(new QueryClient());
 
   return (
-    <QueryClientProvider client={client}>
-      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <StoreProvider>
+      <QueryClientProvider client={client}>
+        <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </StoreProvider>
   );
 }
 
